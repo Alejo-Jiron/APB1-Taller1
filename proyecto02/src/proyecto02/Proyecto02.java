7945558349a0b9aa4329c5c5bf02b52dfa062ea2
Alejandro-Jiron <jajiron1@utpl.edu.ec>
2023-10-23 19:38:54 -0500

    tercer commit. trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author HP
+ */
+public class Proyecto02 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico desde Java Versionado a travez de GIT Manejada desde GITHUB Ademas se usar GitHubClassroom");
+    }
+    
+}
